21050b0240e370c462f01f5f63802bf5b368f1ca
Gilbert l'éclair <lambertb@outook.fr>
2017-02-28 16:07:55 +0100

    création de la première fenetre dite de connexion

--- UserManager/src/usermanager/UserManager.java
+++ UserManager/src/usermanager/UserManager.java
@@ -15,7 +15,12 @@ public class UserManager {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Connection connection = new Connection();
+        
+        connection.setLocationRelativeTo(null);
+        
+        connection.setVisible(true);
     }
     
 }
